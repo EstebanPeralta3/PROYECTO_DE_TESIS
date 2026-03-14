34c6dd98b0e439c1259b84434a3abc2791676224
EstebanPeralta3 <estebanperal22@gmail.com>
2026-03-14 15:40:00 -0300

    IP actualizada

--- mi_aplicacion_web/repositorios/connect_db.py
+++ mi_aplicacion_web/repositorios/connect_db.py
@@ -2,6 +2,6 @@
 from sqlalchemy import create_engine
 
 def connect():
-    engine = create_engine("oracle+oracledb://jbz_docs:1234@192.168.10.102:1521/?service_name=XEPDB1")
+    engine = create_engine("oracle+oracledb://jbz_docs:1234@192.168.245.1:1521/?service_name=XEPDB1")
     return engine
 
